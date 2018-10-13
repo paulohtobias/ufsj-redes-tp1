464f9a6b2c08c7c37f7c2504108d4a48835c48a2
Paulo Tobias <paulohtobias@outlook.com>
2018-10-12 21:20:16 -0300

    Teste 2: chat
    Feito para testar os comandos e comunicação entre os clientes

--- testes/cliente.c
+++ testes/cliente.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define handle_error(cod, msg)\
 	perror(msg); exit(cod);
@@ -15,15 +16,24 @@
 
 #define BUFF_SIZE 1024
 
+int ssfd;
+pthread_t threads[2];
+
+///Lê o log do chat do servidor e exibe na tela.
+void *thread_leitura(void *);
+
+///Envia as mensagens do cliente ao servidor.
+void *thread_escrita(void *);
+
 int main(int argc, char *argv[]) {
+	int i;
 	int retval;
-	int sfd;
 
 	char buff[BUFF_SIZE];
 	
-	sfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sfd == -1) {
-		handle_error(sfd, "socket");
+	ssfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (ssfd == -1) {
+		handle_error(ssfd, "socket");
 	}
 
 	struct sockaddr_in server_addr;
@@ -31,21 +41,50 @@ int main(int argc, char *argv[]) {
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(PORT);
 
-	retval = connect(sfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+	retval = connect(ssfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
 
 	if (retval == -1) {
 		handle_error(retval, "connect");
 	}
 
-	printf("Client %d: connected to server.\n", sfd);
+	printf("[Client] connected to server.\n");
 
-	int i = 0;
-	int st = atoi(argv[2]);
-	while (1) {
-		snprintf(buff, BUFF_SIZE, "carta %s-%02d", argv[1], i++);
-		write(sfd, buff, strlen(buff) + 1);
-		sleep(st);
-	}
+	pthread_create(threads, NULL, thread_escrita, NULL);
+	pthread_create(threads + 1, NULL, thread_leitura, NULL);
+
+	pthread_join(threads[0], NULL);
+	pthread_join(threads[1], NULL);
 
 	return 0;
+}
+
+void *thread_leitura(void *arg){
+	int retval;
+	char buff[BUFF_SIZE];
+	while (1) {
+		retval = read(ssfd, buff, BUFF_SIZE);
+		if (retval == -1) {
+			handle_error(1, "thread_leitura-read");
+		}
+
+		if (retval > 0) {
+			puts(buff);
+		}
+	}
+}
+
+void *thread_escrita(void *arg){
+	int len;
+	char buff[BUFF_SIZE];
+	while (1) {
+		//scanf("%s", buff);
+		len = read(0, buff, BUFF_SIZE);
+		if (len == -1) {
+			handle_error(1, "thread_escrita-read");
+		}
+		buff[len - 1] = '\0';
+		if (write(ssfd, buff, len) == -1) {
+			handle_error(1, "thread_escrita-write");
+		}
+	}
 }
